c35740ca1a804931e30d3bb8d87a4428d30ca0a4
mjroose <mjroose@gmail.com>
2018-02-03 15:20:25 -0500

    Add geolocation functionality

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,16 +1,31 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
-    const user = "Test User";
+    const from = "Test User";
     const text = "Test message";
 
-    var res = generateMessage(user, text);
+    var res = generateMessage(from, text);
 
-    expect(res.from).toBe(user);
+    expect(res.from).toBe(from);
     expect(res.text).toBe(text);
+    expect(typeof res.createdAt).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate the correct location message object', () => {
+    const from = "Test User";
+    const latitude = 75;
+    const longitude = 40;
+    const url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+
+    var res = generateLocationMessage(from, latitude, longitude);
+
+    expect(res.from).toBe(from);
+    expect(res.url).toBe(url);
     expect(typeof res.createdAt).toBe('number');
   });
 });
